dbc3789f876ab76e76d318844152059834c16a10
Daisuke Hayashi <dskhys@outlook.com>
2024-09-03 17:23:33 +0900

    localStorageへ保存

--- src/components/Tiptap.tsx
+++ src/components/Tiptap.tsx
@@ -1,15 +1,32 @@
 "use client";
 
-import { useEditor, EditorContent } from "@tiptap/react";
+import { type Editor, EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { useRef } from "react";
 
+const load = (editor: Editor) => {
+  const content = JSON.parse(localStorage.getItem("content") ?? '""');
+  editor.commands.setContent(content);
+};
+
+const save = (editor: Editor) => {
+  const content = JSON.stringify(editor.getJSON());
+  localStorage.setItem("content", content);
+};
 
 const Tiptap = () => {
   const ref = useRef<HTMLDivElement>(null);
   const editor = useEditor({
     extensions: [StarterKit],
     autofocus: true,
+    onCreate: ({ editor }) => {
+      load(editor);
+    },
+    onUpdate: ({ editor }) => {
+      save(editor);
+    },
+  });
+
   const focus = () => {
     ref.current
       ?.querySelector<HTMLDivElement>("[contenteditable=true]")
