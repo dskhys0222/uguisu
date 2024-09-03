19528c42d862840ae89cbed37f71d019a861f0a8
Daisuke Hayashi <dskhys@outlook.com>
2024-09-03 17:23:21 +0900

    任意の場所をクリックでfocus可能にする

--- src/components/Tiptap.tsx
+++ src/components/Tiptap.tsx
@@ -2,14 +2,28 @@
 
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { useRef } from "react";
+
 
 const Tiptap = () => {
+  const ref = useRef<HTMLDivElement>(null);
   const editor = useEditor({
     extensions: [StarterKit],
-    content: "<p>Hello World! 🌎️</p>",
-  });
+    autofocus: true,
+  const focus = () => {
+    ref.current
+      ?.querySelector<HTMLDivElement>("[contenteditable=true]")
+      ?.focus();
+  };
 
-  return <EditorContent editor={editor} />;
+  return (
+    <EditorContent
+      className="h-full [&>[contenteditable=true]:focus-visible]:outline-none"
+      editor={editor}
+      ref={ref}
+      onClick={focus}
+    />
+  );
 };
 
 export default Tiptap;
