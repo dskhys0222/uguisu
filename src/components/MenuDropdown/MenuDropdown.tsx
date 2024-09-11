ff9428200c3de640d5fae2838b6e82c67afa1d8d
Daisuke Hayashi <dskhys@outlook.com>
2024-09-11 11:28:19 +0900

    MenuDropdownの拡張性のためにtypeによる切り替えをswitch文に変更

--- src/components/MenuDropdown/MenuDropdown.tsx
+++ src/components/MenuDropdown/MenuDropdown.tsx
@@ -7,6 +7,7 @@ import {
   Trigger,
 } from "@radix-ui/react-dropdown-menu";
 import type { MenuDropdownProps } from "./types";
+import { assetNever } from "@/utils/asssertion";
 
 export default function MenuDropdown(props: MenuDropdownProps) {
   const { triggerElement, contents } = props;
@@ -22,19 +23,26 @@ export default function MenuDropdown(props: MenuDropdownProps) {
       </Trigger>
       <Portal>
         <Content className="rounded border bg-white p-2">
-          {indexedContents.map((x) =>
-            x.type === "item" ? (
-              <Item
-                key={x.key}
-                className="cursor-default rounded px-3 py-1 text-gray-700 text-sm hover:bg-gray-100"
-                onClick={x.content.onClick}
-              >
-                {x.content.labelElement}
-              </Item>
-            ) : (
-              <Separator key={x.key} className="my-1 h-[1px] bg-gray-200" />
-            ),
-          )}
+          {indexedContents.map((x) => {
+            switch (x.type) {
+              case "item":
+                return (
+                  <Item
+                    key={x.key}
+                    className="cursor-default rounded px-3 py-1 text-gray-700 text-sm hover:bg-gray-100"
+                    onClick={x.content.onClick}
+                  >
+                    {x.content.labelElement}
+                  </Item>
+                );
+              case "separator":
+                return (
+                  <Separator key={x.key} className="my-1 h-[1px] bg-gray-200" />
+                );
+              default:
+                assetNever(x);
+            }
+          })}
         </Content>
       </Portal>
     </Root>
